b6cacfcc0705e09459389a89bf55858ce5390440
agu <iba.agustin@gmail.com>
2022-12-03 11:59:22 -0300

    day 3 steps

--- 03/day3.js
+++ 03/day3.js
@@ -8,3 +8,26 @@ import { input } from "./day3input.js";
 // wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
 // ttgJtRGJQctTZtZT
 // CrZsJsPPZsGzwwsLwLmpwMDw
+
+// split string in 2
+// determine shared character
+// find the value of the shared character
+// sum all of them
+
+const exampleData = `vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw`;
+
+const exampleArr = exampleData.split("\n");
+
+console.log(
+  exampleArr.map((list) => {
+    console.log(list.length);
+  })
+);
+
+const sent = "abc";
+console.log(sent.charCodeAt(0));
