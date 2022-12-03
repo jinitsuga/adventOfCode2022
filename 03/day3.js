4357f7bfd6389fae69e28fc9561c872d6d5ce808
agu <iba.agustin@gmail.com>
2022-12-03 13:39:00 -0300

    part 1 solved

--- 03/day3.js
+++ 03/day3.js
@@ -2,32 +2,52 @@ import { input } from "./day3input.js";
 
 // Example data:
 
-// vJrwpWtwJgWrhcsFMMfFFhFp
-// jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
-// PmmdzqPrVvPwwTWBwg
-// wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
-// ttgJtRGJQctTZtZT
-// CrZsJsPPZsGzwwsLwLmpwMDw
+// vJrwpWtwJgWrhcsFMMfFFhFp -- p
+// jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL -- L
+// PmmdzqPrVvPwwTWBwg -- P
+// wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn - v
+// ttgJtRGJQctTZtZT -- t
+// CrZsJsPPZsGzwwsLwLmpwMDw -- s
 
 // split string in 2
 // determine shared character
 // find the value of the shared character
 // sum all of them
 
-const exampleData = `vJrwpWtwJgWrhcsFMMfFFhFp
+const data = `vJrwpWtwJgWrhcsFMMfFFhFp
 jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
 PmmdzqPrVvPwwTWBwg
 wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
 ttgJtRGJQctTZtZT
 CrZsJsPPZsGzwwsLwLmpwMDw`;
 
-const exampleArr = exampleData.split("\n");
+let duplicatesList = [];
+let valuesSum = 0;
 
-console.log(
-  exampleArr.map((list) => {
-    console.log(list.length);
-  })
-);
+const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
 
-const sent = "abc";
-console.log(sent.charCodeAt(0));
+const upperAbc = alphabet.map((letter) => {
+  if (letter !== "0") return letter.toUpperCase();
+});
+
+const valuesList = alphabet.concat(upperAbc);
+
+const exampleArr = input.split("\n");
+
+const solutions = exampleArr.map((string) => {
+  const firstPocket = string.split("");
+
+  const secondPocket = firstPocket.splice(0, string.length / 2);
+
+  for (let i = 0; i < firstPocket.length; i++) {
+    const item = firstPocket[i];
+    if (secondPocket.includes(item)) {
+      duplicatesList.push(item);
+      const value = valuesList.indexOf(item) + 1;
+      valuesSum += value;
+      break;
+    }
+  }
+});
+console.log(duplicatesList);
+console.log(valuesSum);
