9ca0c18137251264575e16cea8653971b55edb6e
agu <iba.agustin@gmail.com>
2022-12-12 13:54:55 -0300

    hierarchy tree formed

--- 07/day7.js
+++ 07/day7.js
@@ -24,10 +24,14 @@ $ ls
 7214296 k`;
 
 // FIND DIRECTORIES WITH AT MOST 100.000 In size and sum them.
+// - / (dir)
+//   - a (dir)
+//     - e (dir)
+//   - d (dir)
 
 // Analyze command
 // Keep track of folder we're in - track cd x, cd .. etc
-// 2 collections? Files and Dirs
+// 2 collections? Files and dirs
 // File objects have its name, its size, and the dir they belong to
 // Dir objects have name, dir they're in, references to files they contain, and sub-dirs
 // Check all files and their sizes, adding to the dir size they belong to
@@ -35,24 +39,28 @@ $ ls
 const data = exampleData.split("\n");
 console.log(data);
 
-let dir = 0;
+let tree = [];
 
-let dirTree = [];
-
-let upperDir = "";
-
-let currentDir = dirTree[dir];
+let index = 0;
 
 data.map((command) => {
   let dirName = "";
+  let nestedtree = [];
+  let files = [];
+
   if (command.includes("$ cd ..")) {
-    dir - 1;
+    index--;
   }
   if (command.includes("$ cd ") && !command.includes(" ..")) {
     for (let i = 5; i < command.length; i++) {
       dirName += command[i];
     }
-    console.log(dirName);
+    if (tree[index]) {
+      tree[index] = [...tree[index], dirName];
+    } else {
+      tree.push([dirName]);
+    }
+    index++;
   }
+  console.log(tree);
 });
-console.log(dirTree);
