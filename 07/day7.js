465814d1faf66f2df0b8cba88483bb31002237b0
agu <iba.agustin@gmail.com>
2022-12-14 18:31:53 -0300

    refactored to files w path and dirs wt path

--- 07/day7.js
+++ 07/day7.js
@@ -1,74 +1,69 @@
 import { input, exampleData } from "./day7Input.js";
 
+// `$ cd /
+// $ ls
+// dir a
+// 14848514 b.txt
+// 8504156 c.dat
+// dir d
+// $ cd a
+// $ ls
+// dir e
+// 29116 f
+// 2557 g
+// 62596 h.lst
+// $ cd e
+// $ ls
+// 584 i
+// $ cd ..
+// $ cd ..
+// $ cd d
+// $ ls
+// 4060174 j
+// 8033020 d.log
+// 5626152 d.ext
+// 7214296 k`;
+
+// Don't care about file names, just make anonymous objects { }
+
+const data = exampleData.split("\n");
+
+let currentDir = [];
+
+let files = [];
+
+let dirs = [];
+
+data.map((line) => {
+  if (line[0] === "$") {
+    const split = line.split(" ");
+    if (split[1] == "cd") {
+      if (split[2] == "..") {
+        currentDir.pop();
+      } else if (split[2] == "/") {
+        return;
+      } else {
+        currentDir.push(split[2]);
+      }
+    }
+  } else {
+    const [size, name] = line.split(" ");
+    if (size == "dir") {
+      dirs.push({ size: 0, path: currentDir.join("/") + `/${name}` });
+    } else {
+      files.push({ size: size, name: name, path: currentDir.join("/") });
+    }
+  }
+});
+
+const sizedFiles = files.filter((file) => Number(file.size) <= 100000);
+
+console.log(sizedFiles);
+console.log(dirs);
+
 // Analyze command
 // Keep track of folder we're in - track cd x, cd .. etc
 // 2 collections? Files and dirs
 // File objects have its name, its size, and the dir they belong to
 // Dir objects have name, dir they're in, references to files they contain, and sub-dirs
 // Check all files and their sizes, adding to the dir size they belong to
-
-const data = exampleData.split("\n");
-
-const files = [];
-const directories = [];
-
-let tree = [];
-
-let currentDir = "";
-
-let index = 0;
-
-// Splitting files and directories (ignoring "$ ls" command, probably a mistake)
-
-data.map((command) => {
-  let dirName = "";
-
-  if (command.includes("$ cd ..")) {
-    index--;
-    return;
-  }
-  if (command.includes("$ cd ") && !command.includes(" ..")) {
-    for (let i = 5; i < command.length; i++) {
-      dirName += command[i];
-    }
-    if (tree[index]) {
-      tree[index] = [...tree[index], dirName];
-    } else {
-      tree.push([dirName]);
-    }
-    index++;
-    currentDir = dirName;
-    return;
-  }
-  if (command.includes("dir ")) {
-    for (let i = 4; i < command.length; i++) {
-      dirName += command[i];
-    }
-    directories.push({ name: dirName, parent: currentDir });
-    return;
-  }
-  if (command != "$ ls") {
-    const file = command.split(" ");
-    const fileSize = Number(file[0]);
-    const fileName = file[1];
-
-    const fileData = { name: fileName, size: fileSize, parent: currentDir };
-    files.push(fileData);
-  }
-});
-
-console.log(directories);
-console.log(files);
-
-// Adding size from FILES to dirs
-
-const dirsWithFiles = directories.map((dir) => {
-  let size = 0;
-  const dirSize = files
-    .filter((file) => file.parent === dir.name)
-    .map((file) => (size += file.size));
-  return { ...dir, size: size };
-});
-console.log(dirsWithFiles);
-
-// Adding size from children DIRS to dirs
